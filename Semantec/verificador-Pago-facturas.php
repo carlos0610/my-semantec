1ccb90b8afb7931cb3f16214ce6ba6d6d0aaf548
diegoartesanal@gmail.com <diegoartesanal@gmail.com@3a945f4f-7678-667a-fe27-95a550f1b01f>
2013-02-11 01:52:13 +0000

    Se limpian comentarios que estaban al pedo en verificador-Pago-facturas.php .

--- Semantec/verificador-Pago-facturas.php
+++ Semantec/verificador-Pago-facturas.php
@@ -14,15 +14,15 @@ $n=0;
 ?>
   <script type="text/javascript">
   function mandarFormulario() {
-    //    alert('transfiriendo..');
+    
 	document.getElementById("formVerificadorPagosFactura").submit();
   };
   </script>
 
   <? echo '<big><b>Cargando... </b></big><img src="images/loader.gif"  alt="salir" title="Salir" width="32" height="32" border="none" valign="middle" hspace="8" />'; 
   while ($i < $cantTotal)
-{ include("conexion.php");  // borrrARR!!!!1
-    $i++; //echo $i,'---';
+{ include("conexion.php");  
+    $i++; 
     $CodigoOrden=$_POST["chckCodFactura$i"];
     if($CodigoOrden!=''){       
         $n++; }}
@@ -43,16 +43,12 @@ $n=0;
 $i=0;
 $n=0;
 while ($i < $cantTotal)
-{ //include("conexion.php");  // borrrARR!!!!1
-    $i++; //echo $i,'---';
+{ 
+    $i++; 
     $CodigoOrden=$_POST["chckCodFactura$i"];
     if($CodigoOrden!=''){       
         $n++;
-    //    echo 'envio : ',$n,'>>>',$CodigoOrden;
-    //    $filaFactura = getTotalAPagarConIva($CodigoOrden);
-    //   $facturaTotal = mysql_fetch_array($filaFactura);
-   //    echo "total de factura: ", $facturaTotal["total"];
-  //      echo "nuermo de fac: ",getFacturasCodigoWhitID($CodigoOrden),"<br>";
+    
       
 ?>
        <input type="hidden" name="o<?php echo $n; ?>"  id="o<?php echo $n; ?>" value="<?php echo ($CodigoOrden); ?>" > 
